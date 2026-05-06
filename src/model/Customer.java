e20648aa2f52e3b3c0e57d736c024097e1a367e6
Manjiribhumar <151384793+Manjiribhumar@users.noreply.github.com>
2026-05-07 00:39:42 +0530

    Add Customer class with properties and constructor

---
+++ src/model/Customer.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Customer {
+    public int id;
+    public String name, email;
+
+    public Customer(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
